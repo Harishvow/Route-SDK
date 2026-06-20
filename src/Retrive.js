318aaa0a967f5501f86c97e3840607c8838a7209
Harishvow <harisharish982005@gmail.com>
2026-06-20 18:04:32 +0530

    fix: update RetriveMessage to use POST method for API call

--- src/Retrive.js
+++ src/Retrive.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
 
 async function RetriveMessage(Userapikey, message){
-    const response = await axios.get("http://localhost:5005/api/getApiKey", {
-        data: { Userapikey, message }
+    const response = await axios.post("http://localhost:5005/api/getApiKey", {
+        Userapikey,
+        message
     });
     return response.data;
 }
